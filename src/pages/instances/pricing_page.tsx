c50ee279476d16903782878780b832c32caffa75
Garry Dolley <gdolley@arpnetworks.com>
2024-03-03 22:29:29 -0800

    Integrated Stripe pricing table, added order button, and removed Categories

---
+++ src/pages/instances/pricing_page.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+export const PricingPage = () => {
+  return (
+    <stripe-pricing-table
+      pricing-table-id="prctbl_1OpT1dJLbW0QVEc9uGbT0Q0k"
+      publishable-key="pk_test_51KBYkQJLbW0QVEc9dSkB5oLc2VLDIzxFpsW2IiierSXmIjkgsgaTak02IMBgwTFK3liAej5uZqlbBiG4fDGFmCXY00a99bQYNB"
+    >
+    </stripe-pricing-table>
+  );
+}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'stripe-pricing-table': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    }
+  }
+}
+
+export default PricingPage;
