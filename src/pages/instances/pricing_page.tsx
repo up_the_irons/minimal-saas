b6385b775a88272669ea8402ca1dfc7e8ea2a783
Garry Dolley <gdolley@arpnetworks.com>
2024-03-04 20:18:02 -0800

    Pass customer's email to Stripe's checkout form

--- src/pages/instances/pricing_page.tsx
+++ src/pages/instances/pricing_page.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 
 export const PricingPage = () => {
+  const email = localStorage.getItem("account-email");
+
   return (
     <stripe-pricing-table
       pricing-table-id="prctbl_1OpT1dJLbW0QVEc9uGbT0Q0k"
       publishable-key="pk_test_51KBYkQJLbW0QVEc9dSkB5oLc2VLDIzxFpsW2IiierSXmIjkgsgaTak02IMBgwTFK3liAej5uZqlbBiG4fDGFmCXY00a99bQYNB"
+      customer-email={email}
     >
     </stripe-pricing-table>
   );
